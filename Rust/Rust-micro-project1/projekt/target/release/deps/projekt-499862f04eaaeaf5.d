761e03055943125d5a138d66c4789a4ff8529dae
mhajah <91793810+mhajah@users.noreply.github.com>
2023-11-14 12:03:47 +0100

    Add files via upload

---
+++ Rust/Rust-micro-project1/projekt/target/release/deps/projekt-499862f04eaaeaf5.d
@@ -0,0 +1,5 @@
+D:\Rust-Lectures\Lista4(projekt)\projekt\target\release\deps\projekt-499862f04eaaeaf5.exe: src\main.rs
+
+D:\Rust-Lectures\Lista4(projekt)\projekt\target\release\deps\projekt-499862f04eaaeaf5.d: src\main.rs
+
+src\main.rs:
