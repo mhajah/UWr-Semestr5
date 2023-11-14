291abaac30225367b1fa1ddf74c64d70f3c252d8
mhajah <91793810+mhajah@users.noreply.github.com>
2023-11-14 12:02:22 +0100

    Delete Rust-micro-project1/projekt directory

--- Rust-micro-project1/projekt/target/release/deps/projekt-2e441c247f8df864.d
+++
@@ -1,5 +0,0 @@
-D:\Rust-Lectures\Lista4(projekt)\projekt\target\release\deps\projekt-2e441c247f8df864.exe: src\main.rs
-
-D:\Rust-Lectures\Lista4(projekt)\projekt\target\release\deps\projekt-2e441c247f8df864.d: src\main.rs
-
-src\main.rs:
